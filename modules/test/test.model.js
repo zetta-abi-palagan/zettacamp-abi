045084ac1b9e88fc3237c14056052f0bdbac619e
zettacamp-abi-palagan <abi.palagan@zettacamp.pro>
2025-06-17 13:20:44 +0700

    [BACKEND_MODULE_2][Development 3: Test][BE_019] Mongoose schmea for Test

--- modules/test/test.model.js
+++ modules/test/test.model.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const testSchema = mongoose.Schema({
-    // ID of the block the subject belongs to
+    // ID of the subject the test belongs to
     subject: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Subject",
@@ -21,11 +21,32 @@ const testSchema = mongoose.Schema({
         required: true
     },
 
+    // Type of the test: FREE CONTINUOUS CONTROL, MEMMOIRE ORAL NON JURY, MEMOIRE ORAL, MEMOIRE WRITTEN, MENTOR EVALUATION, ORAL, and WRITTEN
+    test_type: {
+        type: String,
+        enum: ['FREE_CONTINUOUS_CONTROL', 'MEMMOIRE_ORAL_NON_JURY', 'MEMOIRE_ORAL', 'MEMOIRE_WRITTEN', 'MENTOR_EVALUATION', 'ORAL', 'WRITTEN'],
+        required: true
+    },
+
+    // How is the result of the test visible to student: NEVER, AFTER CORRECTION, AFTER JURY DECISION FOR FINAL TRANSCRIPT
+    result_visibility: {
+        type: String,
+        enum: ['NEVER', 'AFTER_CORRECTION', 'AFTER_JURY_DECISION_FOR_FINAL_TRANSCRIPT'],
+        required: true
+    },
+
     // The weight of the test's score in the final subject calculation (e.g., 0.3 for 30%)
     weight: {
         type: Number,
         required: true,
         min: 0
+    },
+
+    // How the test is corrected: ADMTC, CERTIFIER, ACADEMIC CORRECTOR, PREPARATION CENTER
+    correction_type: {
+        type: String,
+        enum: ['ADMTC', 'CERTIFIER', 'ACADEMIC_CORRECTOR', 'PREPARATION_CENTER'],
+        required: true
     },
 
     // An array of grading criteria objects, where each object contains notation_text and max_points
@@ -38,9 +59,22 @@ const testSchema = mongoose.Schema({
         // The maximum score that can be awarded for this criterion
         max_points: {
             type: Number,
-            required: true
+            required: true,
+            min: 0
         }
     }],
+
+    // Check if this test is retake or not
+    is_retake: {
+        type: Boolean,
+        required: true
+    },
+
+    // The test that this test is retake for (only for retake by test)
+    connected_test: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Test"
+    },
 
     // Current status of the test: ACTIVE, INACTIVE, or DELETED
     test_status: {
@@ -60,6 +94,29 @@ const testSchema = mongoose.Schema({
     published_date: {
         type: Date
     },
+
+    //  ID of the user that published this test
+    published_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+
+    // Timestamp for when the test is due
+    test_due_date: {
+        type: Date
+    },
+
+    //  List of student test result's IDs associated with this test
+    student_test_results: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'StudentTestResult'
+    }],
+
+    //  List of task's IDs associated with this test
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Task'
+    }],
 
     // ID of the user who created this test record
     created_by: {
