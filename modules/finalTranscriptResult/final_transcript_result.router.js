6cdb77294619d2303a25b3ccfd43b201e96e8688
zettacamp-abi-palagan <abi.palagan@zettacamp.pro>
2025-07-07 11:51:37 +0700

    [BACKEND_MODULE_4][BE_001] Create new express route: GET /api/transcript/:studentId/pdf

---
+++ modules/finalTranscriptResult/final_transcript_result.router.js
@@ -0,0 +1,12 @@
+// *************** IMPORT CORE ***************
+const express = require('express');
+
+// *************** IMPORT MODULE ***************
+const FinalTranscriptResultController = require('./final_transcript_result.controller');
+
+const router = express.Router();
+
+router.get('/transcript/:studentId/pdf', FinalTranscriptResultController.GetFinalTranscriptPdf);
+
+// *************** EXPORT MODULE ***************
+module.exports = router;
