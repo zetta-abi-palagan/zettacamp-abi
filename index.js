54685978ab3280e96c63a146d951d923b8b8c360
zettacamp-abi-palagan <abi.palagan@zettabyte.life>
2025-09-05 21:15:34 +0700

    update spacing in all file from 4 spaces to 2 spaces indentation

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ const port = config.PORT;
  * It will terminate the application process if any critical step fails.
  */
 async function StartApp() {
-    try {
-        const app = InitializeExpressApp();
-        console.log('Express app initialized.');
+  try {
+    const app = InitializeExpressApp();
+    console.log('Express app initialized.');
 
-        // *************** Connecting to the MongoDB
-        await ConnectDB();
+    // *************** Connecting to the MongoDB
+    await ConnectDB();
 
-        // *************** Setting up the Apollo server
-        await SetupApolloServer(app, port);
+    // *************** Setting up the Apollo server
+    await SetupApolloServer(app, port);
 
-        // *************** Starting the Express server
-        await StartExpressServer(app, port);
-        console.log('Application initialized successfully.');
-    } catch (err) {
-        console.error('Application initialization failed:', err.message);
-        console.error(err.stack);
-        process.exit(1);
-    }
+    // *************** Starting the Express server
+    await StartExpressServer(app, port);
+    console.log('Application initialized successfully.');
+  } catch (err) {
+    console.error('Application initialization failed:', err.message);
+    console.error(err.stack);
+    process.exit(1);
+  }
 }
 
 // *************** Start the application
